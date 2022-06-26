3f05e4a5dcb673e8035101749b3c3c61d16a27f7
Bryan <mhdalana211201@gmail.com>
2022-06-27 01:41:25 +0700

    memperbaiki API Key

--- src/scripts/data/restaurant-source.js
+++ src/scripts/data/restaurant-source.js
@@ -29,7 +29,7 @@ class RestaurantData {
     const response = await fetch(API_ENDPOINT.food, {
       method: "GET",
       headers: {
-        "X-RapidAPI-Key": "16ef4349b2msh3ce44bd12ea0b87p1a4f7djsn7711e7866bf5",
+        "X-RapidAPI-Key": "0d5b945f44msh99dce988816d63ep1f8743jsn48968f893c8c",
         "X-RapidAPI-Host": "yummly2.p.rapidapi.com",
       },
       body: JSON.stringify(data),
